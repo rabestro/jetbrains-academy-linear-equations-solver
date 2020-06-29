56f5343f349561b6cb1d4936bca49b655eeb7377
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-06-29 23:25:12 +0300

    Initial commit

---
+++ src/solver/Application.java
@@ -0,0 +1,27 @@
+package solver;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Application {
+    private final String inputFile;
+    private final String outputFile;
+
+    public Application(String inputFile, String outputFile) {
+        this.inputFile = inputFile;
+        this.outputFile = outputFile;
+    }
+
+    public void run() {
+        var matrix = new Matrix();
+        try {
+            matrix.read(new File(inputFile));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        matrix.print();
+        matrix.solve();
+        matrix.write(new File(outputFile));
+    }
+}
---
+++ src/solver/Main.java
@@ -0,0 +1,9 @@
+package solver;
+
+public class Main {
+    public static void main(String[] args) {
+        new Application(args[1], args[3])
+                .run();
+    }
+}
+
